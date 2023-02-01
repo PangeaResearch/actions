d76a121351d8deb0a15257e37b7a57a062f565c3
eric-pangea <98314134+eric-pangea@users.noreply.github.com>
2023-02-01 17:26:24 +0200

    Update tf_nexus.sh

--- trigger-terraform/tf_nexus.sh
+++ trigger-terraform/tf_nexus.sh
@@ -5,4 +5,4 @@ curl -u $NX_CRED -X 'POST' \
   "https://nexus.dev.pangeabiomed.com/service/rest/v1/script/trigger_terraform/run" \
   -H 'Content-Type: text/plain' \
   -H 'accept: application/json' \
-  -d '$TF_DIR'
+  -d $TF_DIR
