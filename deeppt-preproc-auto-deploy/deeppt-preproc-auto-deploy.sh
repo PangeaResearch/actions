fd64861c338e8931781ecdd95c1bfb719e5a2489
eric-pangea <98314134+eric-pangea@users.noreply.github.com>
2023-02-28 19:19:59 +0200

    Update deeppt-preproc-auto-deploy.sh

--- deeppt-preproc-auto-deploy/deeppt-preproc-auto-deploy.sh
+++ deeppt-preproc-auto-deploy/deeppt-preproc-auto-deploy.sh
@@ -2,6 +2,6 @@
 
 
 curl -u $NX_CRED -X 'POST' \
-  "https://nexus.dev.pangeabiomed.com/service/rest/v1/tasks/deeppt-preproc-auto-deploy/run" \
+  "https://nexus.dev.pangeabiomed.com/service/rest/v1/script/deeppt-preproc-auto-deploy/run" \
   -H 'Content-Type: text/plain' \
   -H 'accept: application/json'
