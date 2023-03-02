ef402c9ab4ce08a5a65725a081ac2e3d277cb859
eric-pangea <98314134+eric-pangea@users.noreply.github.com>
2023-03-02 16:55:43 +0200

    Update ecr_auto_deploy.sh

--- trigger-ecr-auto/ecr_auto_deploy.sh
+++ trigger-ecr-auto/ecr_auto_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl -v -u $NX_CRED -X 'POST' \
+curl -u $NX_CRED -X 'POST' \
   "https://nexus.dev.pangeabiomed.com/service/rest/v1/script/ecr-auto-deploy/run" \
   -H 'Content-Type: text/plain' \
   -H 'accept: application/json' \
