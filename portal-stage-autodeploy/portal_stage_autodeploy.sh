a158d4126d96ba1d4213a1c545b91405ebfa3f0e
eric-pangea <98314134+eric-pangea@users.noreply.github.com>
2024-01-23 13:35:07 +0200

    Update portal_stage_autodeploy.sh

--- portal-stage-autodeploy/portal_stage_autodeploy.sh
+++ portal-stage-autodeploy/portal_stage_autodeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -u "$NX_CRED" -X -v 'POST' \
+curl -u "$NX_CRED" -X -vvv 'POST' \
   "https://nexus.dev.pangeabiomed.com/service/rest/v1/script/portal-stage-deploy/run" \
   -H 'Content-Type: text/plain' \
   -H 'accept: application/json' \
