3c4b8d5580427a5597a77992db6a846d6f5521cb
eric-pangea <98314134+eric-pangea@users.noreply.github.com>
2024-01-23 14:08:50 +0200

    Update portal_stage_autodeploy.sh
    added trim

--- portal-stage-autodeploy/portal_stage_autodeploy.sh
+++ portal-stage-autodeploy/portal_stage_autodeploy.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+ #!/bin/bash
 
-STAGE_DATA=$(echo "$1" | tr -d "[:space:]" ),$("$2" | tr -d "[:space:]"),$("$3" | tr -d "[:space:]")
+STAGE_DATA=$(echo "$1" | tr -d "[:space:]" ),$(echo "$2" | tr -d "[:space:]"),$(echo "$3" | tr -d "[:space:]")
 echo "the stage data is =$STAGE_DATA="
 curl -u "$NX_CRED" -X -v 'POST' \
   "https://nexus.dev.pangeabiomed.com/service/rest/v1/script/portal-stage-deploy/run" \
